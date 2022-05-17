78e8104fae14615bb2d768872337419eee933781
Nikolay Smekalin <nnsmekalin@domclick.ru>
2022-05-17 21:03:19 +0300

    update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,7 +11,6 @@ const router = express.Router();
 
 router.post(
   '/registration',
-  roleMiddleware([ROLE_ADMIN]),
   body('email').isEmail(),
   body('password').isLength({ min: 3, max: 32 }),
   AuthController.register,
